28d1eea92c6f7e4d4a4688c4050fa6a6776d0fab
rod773 <rodrigoarcadio1@gmail.com>
2026-03-01 11:36:54 +0100

    footer section shadch tailark

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import FAQsThree from "@/components/faqs-3";
 import Features from "@/components/features-3";
+import FooterSection from "@/components/footer";
 import HeroSection from "@/components/hero-section";
 import IntegrationsSection from "@/components/integrations-3";
 import StatsSection from "@/components/stats-3";
 import TeamSection from "@/components/team";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -15,6 +15,7 @@ export default function Home() {
       <StatsSection />
       <TeamSection />
       <FAQsThree />
+      <FooterSection />
     </div>
   );
 }
