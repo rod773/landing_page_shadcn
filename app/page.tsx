32db8a0c224d6674f5b65b0034bf25fbeea472a4
rod773 <rodrigoarcadio1@gmail.com>
2026-03-01 11:33:50 +0100

    more sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import FAQsThree from "@/components/faqs-3";
 import Features from "@/components/features-3";
 import HeroSection from "@/components/hero-section";
 import IntegrationsSection from "@/components/integrations-3";
 import StatsSection from "@/components/stats-3";
+import TeamSection from "@/components/team";
 import Image from "next/image";
 
 export default function Home() {
@@ -11,6 +13,8 @@ export default function Home() {
       <Features />
       <IntegrationsSection />
       <StatsSection />
+      <TeamSection />
+      <FAQsThree />
     </div>
   );
 }
