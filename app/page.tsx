628ca940d82c79b89904f1c6ac719d556a940980
rod773 <rodrigoarcadio1@gmail.com>
2026-03-01 11:21:39 +0100

    ntegrationsSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Features from "@/components/features-3";
 import HeroSection from "@/components/hero-section";
+import IntegrationsSection from "@/components/integrations-3";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <HeroSection />
       <Features />
+      <IntegrationsSection />
     </div>
   );
 }
